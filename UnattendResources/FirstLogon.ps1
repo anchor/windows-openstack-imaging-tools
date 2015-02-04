c45ba3a62848ef7407ef901141524d9855ee2391
Craige McWhirter <craige@mcwhirter.com.au>
2015-02-04 14:47:02 +1100

    Completed merge of upstream and Anchor

--- UnattendResources/FirstLogon.ps1
+++ UnattendResources/FirstLogon.ps1
@@ -1,24 +1,86 @@
 $ErrorActionPreference = "Stop"
-$resourcesDir = "$ENV:SystemDrive\UnattendResources"
 
-function getHypervisor() {
-    $hypervisor = & "$resourcesDir\checkhypervisor.exe"
+function getOSVersion(){
+    $v = (Get-WmiObject Win32_OperatingSystem).Version.Split('.')
 
-    if ($LastExitCode -eq 1) {
-        Write-Host "No hypervisor detected."
-    } else {
-        return $hypervisor
+    return New-Object psobject -Property @{
+        Major = [int]::Parse($v[0])
+        Minor = [int]::Parse($v[1])
+        Build = [int]::Parse($v[2])
     }
 }
 
+function getVirtioDriversFolder(){
+    $architectureMapping = @{}
+    $architectureMapping['32-bit']='X86'
+    $architectureMapping['64-bit']='AMD64'
+    $osVersionMapping = @{}
+    $osVersionMapping[0]='VISTA'
+    $osVersionMapping[1]='WIN7'
+    $osVersionMapping[2]='WIN8'
+    $osVersionMapping[3]='WIN8'
+
+    $osArchitecture = (Get-WmiObject Win32_OperatingSystem).OSArchitecture
+    $archFolder = $architectureMapping[$osArchitecture]
+
+    $osVersion = getOSVersion
+    $versionFolder = $osVersionMapping[$osVersion.Minor]
+    if (($osVersion.Major -ne 6) -or !$versionFolder) { throw "Unsupported Windows version" }
+
+    $virtIOPath = Join-Path -Path $versionFolder -ChildPath $archFolder
+    $drive = (gwmi Win32_CDROMDrive | where {(Test-Path (join-path -Path $_.Drive -ChildPath $virtIOPath ))}).Drive
+    if (! $drive) { throw "VirtIO drivers not found" }
+
+    return join-path -Path $drive -ChildPath $virtIOPath | join-path -ChildPath "*.inf"
+}
+
+function installVirtIOTools2012($virtioDriversPath) {
+    $Host.UI.RawUI.WindowTitle = "Downloading VirtIO drivers script..."
+    $virtioScriptPath = "$ENV:Temp\InstallVirtIODrivers.js"
+    $url = "$baseUrl/InstallVirtIODrivers.js"
+    (new-object System.Net.WebClient).DownloadFile($url, $virtioScriptPath)
+
+    Write-Host "Installing VirtIO drivers from: $virtioDriversPath"
+    & cscript $virtioScriptPath $virtioDriversPath
+    if (!$?) { throw "InstallVirtIO failed" }
+    del $virtioScriptPath
+}
+
+function installVirtIOToolsPre2012($virtioDriversPath) {
+    $Host.UI.RawUI.WindowTitle = "Downloading VirtIO certificate..."
+    $virtioCertPath = "$ENV:Temp\VirtIO.cer"
+    $url = "$baseUrl/VirtIO.cer"
+    (new-object System.Net.WebClient).DownloadFile($url, $virtioCertPath)
+
+    $Host.UI.RawUI.WindowTitle = "Installing VirtIO certificate..."
+    $cacert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($virtioCertPath)
+    $castore = New-Object System.Security.Cryptography.X509Certificates.X509Store([System.Security.Cryptography.X509Certificates.StoreName]::TrustedPublisher,`
+                     [System.Security.Cryptography.X509Certificates.StoreLocation]::LocalMachine)
+    $castore.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
+    $castore.Add($cacert)
+
+    Write-Host "Installing VirtIO drivers from: $virtioDriversPath"
+    Start-process -Wait pnputil "-i -a $virtioDriversPath"
+    if (!$?) { throw "InstallVirtIO failed" }
+
+    del $virtioCertPath
+    $castore.Remove($cacert)
+}
+
+$logonScriptPath = "$ENV:SystemRoot\Temp\Logon.ps1"
+
 try
 {
-    $hypervisorStr = getHypervisor
-    Write-Host "Hypervisor: $hypervisorStr"
+    $Host.UI.RawUI.WindowTitle = "Downloading Logon script..."
+    $baseUrl = "https://raw.github.com/anchor/windows-openstack-imaging-tools/Anchor"
+    (new-object System.Net.WebClient).DownloadFile("$baseUrl/Logon.ps1", $logonScriptPath)
+
+    $virtPlatform = (gwmi Win32_ComputerSystem).Manufacturer
+    Write-Host "Virtual platform: $virtPlatform"
     # TODO: Add XenServer / XCP
-    switch($hypervisorStr)
+    switch($virtPlatform)
     {
-        "VMwareVMware"
+        "VMware, Inc."
         {
             # Note: this command will generate a reboot.
             # "/qn REBOOT=ReallySuppress" does not seem to work properly
@@ -26,13 +88,19 @@ try
             E:\setup64.exe `/s `/v `/qn `/l `"$ENV:Temp\vmware_tools_install.log`"
             if (!$?) { throw "VMware tools setup failed" }
         }
-        "KVMKVMKVM"
+        "QEMU"
         {
-            # Nothing to do as VirtIO drivers have already been provisioned
-        }
-        "Microsoft Hv"
-        {
-            # Nothing to do
+            $virtioDriversPath = getVirtioDriversFolder
+            $osVersion = getOSVersion
+
+            if (($osVersion.Major -ge 6) -and ($osVersion.Minor -ge 2)) {
+                installVirtIOTools2012 $virtioDriversPath
+            }
+            else {
+                installVirtIOToolsPre2012 $virtioDriversPath
+            }
+
+            shutdown /r /t 0
         }
     }
 }
@@ -41,8 +109,6 @@ catch
     $host.ui.WriteErrorLine($_.Exception.ToString())
     $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
     # Prevents the setup from proceeding
-
-    $logonScriptPath = "$resourcesDir\Logon.ps1"
     if ( Test-Path $logonScriptPath ) { del $logonScriptPath }
     throw
 }
--- UnattendResources/Specialize.ps1
+++ UnattendResources/Specialize.ps1
@@ -1,33 +1,41 @@
 $ErrorActionPreference = "Stop"
-$resourcesDir = "$ENV:SystemDrive\UnattendResources"
 
 try
 {
-    $wallpaper = "$resourcesDir\Wallpaper.png"
-    if(Test-Path $wallpaper)
+    $temp = "$ENV:SystemRoot\Temp"
+    $baseUrl = "https://raw.github.com/anchor/windows-openstack-imaging-tools/Anchor"
+
+    # Put the wallpaper in place
+    $wallpaper_dir = "$ENV:SystemRoot\web\Wallpaper\Cloudbase"
+    if (!(Test-Path $wallpaper_dir))
     {
-        $Host.UI.RawUI.WindowTitle = "Configuring wallpaper..."
-
-        # Put the wallpaper in place
-        $wallpaper_dir = "$ENV:SystemRoot\web\Wallpaper\Cloudbase"
-        if (!(Test-Path $wallpaper_dir))
-        {
-            mkdir $wallpaper_dir
-        }
-
-        copy "$wallpaper" "$wallpaper_dir\Wallpaper-Cloudbase-2013.png"
-        $gpoZipPath = "$resourcesDir\GPO.zip"
-        foreach($item in (New-Object -com shell.application).NameSpace($gpoZipPath).Items())
-        {
-            $yesToAll = 16
-            (New-Object -com shell.application).NameSpace("$ENV:SystemRoot\System32\GroupPolicy").copyhere($item, $yesToAll)
-        }
+        mkdir $wallpaper_dir
     }
+
+    $Host.UI.RawUI.WindowTitle = "Downloading wallpaper..."
+    $wallpaper = "Wallpaper-Cloudbase-2013.png"
+    (new-object System.Net.WebClient).DownloadFile("$baseUrl/$wallpaper", "$wallpaper_dir\$wallpaper")
+
+    # Put the Anchor bmp in place
+    $bmp_dir = "$ENV:SystemRoot\OEM"
+    if (!(Test-Path $bmp_dir))
+    {
+        mkdir $bmp_dir
+    }
+
+    $Host.UI.RawUI.WindowTitle = "Downloading bmp..."
+    $bmp = "logo.bmp"
+    (new-object System.Net.WebClient).DownloadFile("$baseUrl/$bmp", "$bmp_dir\$bmp")
 
     # Enable ping (ICMP Echo Request on IPv4 and IPv6)
     # TODO: replace with with a netsh advfirewall command
     # possibly avoiding duplicates with "File and printer sharing (Echo Request - ICMPv[4,6]-In)"
     netsh firewall set icmpsetting 8
+
+    $Host.UI.RawUI.WindowTitle = "Downloading FirstLogon script..."
+    #Make sure that in case of exception the FirstLogon script will not be executed.
+    #Being the last instruction this is already ok
+    (new-object System.Net.WebClient).DownloadFile("$baseUrl/FirstLogon.ps1", "$temp\FirstLogon.ps1")
 }
 catch
 {
